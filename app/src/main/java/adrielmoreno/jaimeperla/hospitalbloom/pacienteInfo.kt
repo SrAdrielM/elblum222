5186c8d9709158e0a4ec1fc52475f2167fee0ac1
SrAdrielM <20230542@ricaldone.edu.sv>
2024-07-26 17:14:02 -0600

    agregacion de layoputs

---
+++ app/src/main/java/adrielmoreno/jaimeperla/hospitalbloom/pacienteInfo.kt
@@ -0,0 +1,20 @@
+package adrielmoreno.jaimeperla.hospitalbloom
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class pacienteInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_paciente_info)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
